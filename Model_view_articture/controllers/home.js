f9d0a7d148c876889b98c60973aa3456da170814
dubeycode <dubeysatyam3635@gmail.com>
2025-09-20 03:10:38 +0530

    add models

--- Model_view_articture/controllers/home.js
+++ Model_view_articture/controllers/home.js
@@ -1,7 +1,5 @@
 
-
-// array 
-const registerHome=[]
+const Home =require("../models/home")
 
 // add home logic 
 exports.getAddHome=(req,res,next)=>{
@@ -11,19 +9,22 @@ exports.getAddHome=(req,res,next)=>{
 //  addhome logic
 exports.postAddHome=(req, res, next) => {
   console.log('Home Registration successful for:', req.body);
-  registerHome.push(req.body);
+  const {housename,price,location,rating,photo}=req.body;
+
+  const home = new Home(housename,price,location,rating,photo);
+
+  home.save()
   res.render('home_Add', {pageTitle: 'Home Added Successfully', currentPage: 'homeAdded'});
 };
 
 // home page logic
 exports.getHomes=(req,res,next)=>{
-  console.log(registerHome)
-   res.render("home", {
+  Home.featchAll((homes)=>res.render("home", {
     pageTitle: "airbnb Home",
-    registeredHomes: registerHome,
+    registeredHomes: homes,
     currentPage:'Home'
-
-  });
-}
+  })
+);   
+};
 
 
---
+++ Model_view_articture/models/home.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../utils/pathUtil');
+
+
+module.exports = class Home{
+  constructor(housename,price,location,rating,photo){
+    this.housename=housename;
+    this.price=price;
+    this.location=location;
+    this.rating=rating;
+    this.photo=photo;
+  }
+  save(){
+    Home.featchAll((registerHome)=>{
+    registerHome.push(this);
+    const HomeDataPath =path.join(rootDir,'data' ,'homes.json');
+    fs.writeFile(HomeDataPath,JSON.stringify(registerHome),error =>{
+      console.log(error)
+    })
+    })
+   
+  }
+
+  static featchAll(callback){
+    const homeDataPath = path.join(rootDir,'data','homes.json');
+    fs.readFile(homeDataPath,(err,data)=>{
+      // console.log("file read",err,data);
+      callback(!err ? JSON.parse(data):[]);
+    });
+  }
+}
