439d900d7f2e971c1b0b6c97be2bc7aa7a14f6ee
dubeycode <dubeysatyam3635@gmail.com>
2025-10-15 04:30:55 +0530

    opration todo connect with db

--- mongoose/main.js
+++ mongoose/main.js
@@ -1,5 +1,21 @@
 import mongoose from "mongoose";
+import express from "express";
+import { Todo } from "../mongoose/models/Todo.js";
+
+
+let conn = await mongoose.connect("mongodb://localhost:27017/todo")
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  const todo =new Todo({title:"coding full night",desc:"full night without any intraption", isDone:false})
+  todo.save()
+  res.send('Hello World!')
+})
 
 
 
-await mongoose.connect("mongodb://localhost:27017/")
+app.listen(port, () => {
+  console.log(` http://localhost:${port}`)
+})
