cee542f9755f45faab7847d86692ca3d67e08c73
dubeycode <dubeysatyam3635@gmail.com>
2025-10-16 00:13:58 +0530

    Tailwind css understanding

---
+++ Ex_tailwind/tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#6366f1",  // custom color
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
